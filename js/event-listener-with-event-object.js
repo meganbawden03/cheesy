a920b030f62f0bd66016090da1bc160601c8a31e
meganbawden03 <155792004+meganbawden03@users.noreply.github.com>
2024-01-30 15:56:44 -0800

    Add files via upload

---
+++ js/event-listener-with-event-object.js
@@ -0,0 +1,51 @@
+var op = document.getElementById("openPage");
+var tp = document.getElementById("tyPage");
+var submitButton = document.getElementById("submit");
+var signupButton = document.getElementById("signup");
+submitButton.addEventListener("click", function() {
+	event.preventDefault();
+    //alert("Hello! I am an alert box!!");   
+	op.style.display = "none";
+	tp.style.display = "block";
+	
+	
+  });
+
+
+
+
+
+
+function checkLength(e, minLength) {         // Declare function
+  var el, elMsg;                             // Declare variables
+  if (!e) {                                  // If event object doesn't exist
+    e = window.event;                        // Use IE fallback
+  }
+  el = e.target || e.srcElement;             // Get target of event
+  elMsg = el.nextSibling;                    // Get its next sibling
+
+  if (el.value.length < minLength) {         // If length is too short set msg
+    elMsg.innerHTML = 'Username must be ' + minLength + ' characters or more';
+	
+  } else {                                   // Otherwise
+    elMsg.innerHTML = '';                    // Clear message
+  }
+}
+
+var elUsername = document.getElementById('username');// Get username input
+if (elUsername.addEventListener) {           // If event listener supported
+  elUsername.addEventListener('blur', function(e) {  // On blur event
+    // NOTE: This function is checkLength() - not checkUsername()
+    checkLength(e, 5);                             // Call checkLength()
+	
+  }, false);                                       // Capture in bubble phase
+} else {                                           // Otherwise
+  elUsername.attachEvent('onblur', function(e) {   // IE fallback onblur
+    // NOTE: This function is checkLength() - not checkUsername()
+    checkLength(e, 5);                             // Call checkLength()
+  });
+}
+
+
+
+
